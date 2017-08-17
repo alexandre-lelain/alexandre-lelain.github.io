7bec80440f2c2dda681d1ede0912aeb245f80518
Alexandre Le Lain <alexandre.inspiral@gmail.com>
2017-08-17 15:16:40 +0100

    back to top button added

--- js/functions.js
+++ js/functions.js
@@ -14,3 +14,16 @@ function setRandomColor(e) {
 function hideCollapse(){
   $("#myNavbar").collapse('hide');
 }
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("btnTop").style.display = "block";
+    } else {
+        document.getElementById("btnTop").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
