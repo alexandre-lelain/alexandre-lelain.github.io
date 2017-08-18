cf64737248079192875563c031305f82edbe2dd8
Alexandre Le Lain <alexandre.inspiral@gmail.com>
2017-08-18 11:50:32 +0100

    Metadata added

---
+++ js/metadata.js
@@ -0,0 +1,9 @@
+{
+  "@context": "http://schema.org/",
+  "@type": "WebSite",
+  "name": "Alexandre Le Lain's website",
+  "author": "Alexandre Le Lain",
+  "image": "../res/me.png",
+  "description": "My personnal website and projects.",
+  "creator": "Alexandre Le Lain",
+}
