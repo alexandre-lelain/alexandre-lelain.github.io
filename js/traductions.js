2255980c6c389c56e22aa0347d4f03272b9a4e16
Alexandre Le Lain <alexandre.inspiral@gmail.com>
2017-08-18 11:06:37 +0100

    update cv trad

--- js/traductions.js
+++ js/traductions.js
@@ -14,7 +14,7 @@ function saveLangEn(){
 
 function tradFr(){
   $('h1').text("Bienvenue chez moi.");
-  $('.cv a').text("Mon Site Web");
+  $('.cv a').text("Mon CV");
   $('.agent-web a').text("Agent Web");
   $('.top-shape a').text("Top Shape");
   $('.more-projects a').text("Projets en cours...");
@@ -23,7 +23,7 @@ function tradFr(){
 
 function tradEn(){
   $('h1').text("Welcome to my world.");
-  $('.cv a').text("My Website");
+  $('.cv a').text("My Resume");
   $('.agent-web a').text("Web Agent");
   $('.top-shape a').text("Top Shape");
   $('.more-projects a').text("More projects soon...");
