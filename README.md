dfdb05b27bfbb085962964f0990b1c5c274cc28e
Alexandre Le Lain <lelain.alexandre@gmail.com>
2019-12-10 01:18:00 +0100

    build(deploy): deploy v$1

--- README.md
+++
@@ -1,7 +0,0 @@
-# Home
-
-This project is the entry point of all my other projects.
-
-Please visit it on [alexandre-lelain.github.io](https://alexandre-lelain.github.io).
-
-Made with ❤ with React.
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Header from './Header'
-import Intro from './Intro'
-import Projects from './Projects'
-import BackToTopBtn from './BackToTopBtn'
-
-export { Header, Intro, Projects, BackToTopBtn }
